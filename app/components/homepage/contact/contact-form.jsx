1f64b3504187e02d0b5ed9b55c277c799643a1ea
digitalgenieji <145273871+digitalgenieji@users.noreply.github.com>
2024-03-27 01:37:26 +0530

    Update contact-form.jsx

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -34,9 +34,9 @@ function ContactForm() {
       setError({ ...error, required: false });
     };
 
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
+     const serviceID = "service_n223scq";
+    const templateID = "template_n1rikum";
+    const options = { publicKey:"-eQAwEdBR7SAAiY1b"};
 
     try {
       const res = await emailjs.send(serviceID, templateID, input, options);
